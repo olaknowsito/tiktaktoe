13caec8902ec58454aeeda78d6e62abd49b85448
Christopher Olano <colano@edukasyon.ph>
2020-07-26 23:23:07 +0800

    added db config

--- model/Classes/ConnectDb.php
+++ model/Classes/ConnectDb.php
@@ -3,7 +3,7 @@
 		private $con;
 
 		public function dbConnection() {
-			$this->con = mysqli_connect("localhost", "root", "", "babymama");
+			$this->con = mysqli_connect("d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "yq81l3ubfab8m3na", "utahtcg5ni58z09x", "w2y4gwabtqrm6uzk");
 			return $this->con;
 		}
 	}
